a3bcfed7347133972fcd8db48d182cde25d991ac
matHuston <thecaranar@gmail.com>
2026-01-23 15:33:09 -0500

    reflection comment edit

--- 03/Lab_02_Maintainable_and_Scalable/index.php
+++ 03/Lab_02_Maintainable_and_Scalable/index.php
@@ -10,5 +10,5 @@ require 'includes/footer.php';
 
 /* 
 One thing I learned from this lab that I will apply in Course Project Phase One: 
-    I learned that I can break up HTML markup into individual .php files and call them to structure a webpage more succinctly and neatly. I like being able to do this becuase one of my favorite CSS features is being able to stack stylesheets on top of the other like building blocks inside of <head>, which this strategy is pretty similar to. Not only will I make use of this in the Course Project, but I will probably use this strategy consistently when building future sites for other projects.
+    I learned that I can break up HTML markup into individual .php files and call them to dynamically generate a webpage succinctly and neatly. I like being able to do this becuase one of my favorite CSS features is being able to stack stylesheets on top of the other like building blocks inside of <head>, which this strategy is pretty similar to. Not only will I make use of this in the Course Project, but I will probably use this strategy consistently when building future sites for other projects.
 */
