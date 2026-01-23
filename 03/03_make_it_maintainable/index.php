7623d56ad6de0c62ad5ca7877ff63079be6c4f05
matHuston <thecaranar@gmail.com>
2026-01-23 15:25:14 -0500

    reflection comment added

--- 03/03_make_it_maintainable/index.php
+++ 03/03_make_it_maintainable/index.php
@@ -7,3 +7,8 @@ require_once 'includes/connect.php';
 include 'includes/list.php';
 // required to correctly end display of page due to ending header.php <html>
 require 'includes/footer.php';
+
+/* 
+One thing I learned from this lab that I will apply in Course Project Phase One: 
+    I learned that I can break up HTML markup into individual .php files and call them to structure a webpage more succinctly and neatly. I like being able to do this becuase one of my favorite CSS features is being able to stack stylesheets on top of the other like building blocks inside of <head>, which this strategy is pretty similar to. Not only will I make use of this in the Course Project, but I will probably use this strategy consistently when building future sites for other projects.
+*/
