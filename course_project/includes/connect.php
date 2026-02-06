ce4a18646517087ec47540c6c3ade45cc7daad77
matHuston <thecaranar@gmail.com>
2026-02-06 16:53:10 -0500

    connection checking added

--- course_project/includes/connect.php
+++ course_project/includes/connect.php
@@ -1,19 +1,19 @@
 <?php 
 $host = "localhost"; //hostname
-$db = "bitumi"; //database name
+$db = "pro_bending"; //database name
 $user = "root"; //username
 $password = ""; //password
 
 //points to the database mysql serve (driver), database name, username and password
 $dsn = "mysql:host=$host;dbname=$db";
 
-//try to connect, if connected echo a yay!
+// connection try and catch block to handle errors during connection, echo successful connection message or error message
 try {
    $pdo = new PDO ($dsn, $user, $password); 
    //set the error mode to exception so that we can catch any errors that occur during the connection process
    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 }
-//what happens if there is an error connecting 
+//error connecting?
 // die() is a function that stops the script and outputs a message
 // $e is the exception object that contains information about the error that occurred
 // $e->getMessage() is a method that returns the error message associated with the exception
