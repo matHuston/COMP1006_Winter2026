300725235b2b2ec2d5f4851b1908bae127562ecf
matHuston <thecaranar@gmail.com>
2026-02-20 00:47:34 -0500

    review/retrospective + comment clarity

--- course_project/index.php
+++ course_project/index.php
@@ -6,7 +6,8 @@
   <!-- bootstrap - class=mb-4 adds margin-bottom -->
   <form action="process.php" method="post">
 
-    <!-- 
+  <!--  
+  Application One : Team Tracker 
   REQUIREMENTS
     1.) using PHP, the ability for users to add, view, delete and update information 
       obtained via an HTML form and store in a MySQL database 
@@ -21,16 +22,19 @@
       challenges/success/next steps. This can be submitted as a written document or 
       a video recording. 
       
-  Application One : Team Tracker 
   My version of this application is built around Pro-Bending from the Avatar: The Last Airbender universe, because I don't really watch sports but I wanted to do something fun. 
-  Pro-Bending is a 3v3 team-based sport but doesn't have traditional "positions" like other sports, instead each team is comprised of 3 players that can control one of three natural elements; one water-bender, one earth-bender, and one fire-bender. For this reason, my "position" column has been renamed to "bender_element".
+  Pro-Bending is a 3v3 team-based sport but doesn't have traditional "positions" like real-life sports, instead each team is comprised of 3 players that can control one of three natural elements; one water-bender, one earth-bender, and one fire-bender. For this reason, my "position" column has been renamed to "bender_element".
   I had a lot of fun with this project and theming it around one of my favorite shows, and like in my personal database for the project I made up phone numbers and email addresses for different characters that reference little niche details from the show. I know you can't see those but it was fun anyway. Also coming up with silly references like calling emails "hawkmails" because of the messenger dragon-hawks in the world while also being a pun about Hotmail.
+  The main challenge I had was honestly with controlling the CSS of my pages and not related to the PHP. I consider myself to be pretty good at HTML and CSS but I struggled with getting a lot of the custom CSS that I wanted to include working, which is the reason why my pages are still all white and kinda bare-bones. Luckily I was able to include the fonts that I wanted at the very least and control the layout of the header, but only barely. I'm not sure if it was because of Bootstrap or because of something related to XAMPP, because when I used an in-browser feature to force dark mode onto my page one time it also surprisingly changed my XAMPP phpmyadmin pages to dark mode, which was odd because I know that this browser feature only affects the page that you're on and not every page in your browser. Either way I really want to figure the CSS out. When I continue working on this, which is what I assume the part 2 of the course project will be, I would love if I could get more control over my styles and really make my site shine like I wanted it to. 
+  I honestly really loved working on this site, I got into a like vibe-coding headspace while working on it and I surprised myself with how much I enjoyed working with PHP and SQL together. It really lets me see how all the different classes in the my course come together to make a functional web app which I really appreciate. I'm a very visual and physical learner so seeing everything work in tandem to make this site interactable was really cool and rewarding, and helps me understand the real-world application of things like SQL.
+
   The Team Tracker application allows admins to:
     - manage and keep track of their team members 
     - add first name, last name, position (bender_element), telephone number, hawkmail (email) and team name for each player
     - view all team member information
     - provide users with the ability to update team member information as well as delete team members
   -->
+  
 
     <fieldset>
       <!-- "form-label" for labels, "form-control" for inputs, and "form-select" for dropdown select menus -->
